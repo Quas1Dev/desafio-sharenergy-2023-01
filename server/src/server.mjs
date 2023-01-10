1590790dfa82200c7177a330bcc1246cec49c517
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-10 18:37:30 -0300

    Adding our first route.

---
+++ server/src/server.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+// Rote to index
+app.get("/login", (req, resp)=>{
+    console.log("Acessou login.");
+
+    return resp.json([
+        {id: 1, name: "Anúncio 1"},
+        {id: 2, name: "Anúncio 2"},
+        {id: 3, name: "Anúncio 3"},
+    ]);
+})
+
+app.listen(3333);
