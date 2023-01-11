c47bf60d38026d59e80acd532d4031dfd95b7190
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-11 01:12:45 -0300

    Login page created successfully.

---
+++ web/src/components/loginform.tsx
@@ -0,0 +1,55 @@
+import LoginField from "./loginfield";
+import { useState } from "react";
+
+function LoginForm() {
+    let [form, setForm] = useState({
+        usuario: "",
+        senha: "",
+        keepon: false,
+    });
+
+    function handleChange(e: any) {
+        let { name, value, type, checked } = e.target;
+
+        setForm(prevForm => ({
+            ...prevForm,
+            [name]: type === "checkbox" ? checked : value
+        }))
+    }
+
+    return (
+        <main className="login_box">
+            <h1 className="login_box--title">Login</h1>
+            <form action="" className="login_box--login_form">
+                <div>
+                    <label htmlFor="usuario" className="login_form--field_name">Usuário:</label>
+                    <input type="text"
+                        name="usuario"
+                        className="login_form--text_field"
+                        onChange={handleChange}
+                        value={form.usuario} />
+                </div>
+
+                <div>
+                    <label htmlFor="senha" className="login_form--field_name">Senha:</label>
+                    <input type="password"
+                        name="senha"
+                        className="login_form--text_field"
+                        value={form.senha}
+                        onChange={handleChange} />
+                </div>
+
+                <div className="login_form--keepon">
+                    <input type="checkbox" name="keepon" />
+                    <label htmlFor="keepon"
+                        className="keepon--label"
+                        value={form.keepon}
+                        onChange={handleChange}>Mantenha-me conectado</label>
+                </div>
+                <button className="login_form--submit">Enviar</button>
+            </form>
+        </main>
+    )
+}
+
+export default LoginForm;
