8a7a28fe6f412f70afcf95b1687a76d3edc5fd68
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-13 04:42:29 -0300

    Added code to use cors and express.json.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,27 @@
-import express from 'express';
-import { Request, Response } from "express";
+import express, { Request, Response }  from 'express';
+import cors from 'cors'
 
 // This guarantee the dbConfig content gets executed
 import './config/dbConfig';
 import ModelForUser from './models/UsersData';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Check credentials
 app.get("/confirmLogin", async (req: Request, resp: Response)=>{
-    let userExist = await ModelForUser.exists({user: req.query.user, password: req.query.password})
+    let userExist = await ModelForUser.exists({user: req.query.user, password: req.query.password});
 
     if (userExist) {
-        return resp.json({isRegistered: true})
+        return resp.json({isRegistered: true});
     }
 
     return resp.json({isRegistered: false});
+})
+
+app.get("/", (req, resp)=>{
+    return resp.send("Hello");
 })
 
 app.listen(3333);
