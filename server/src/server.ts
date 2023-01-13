3ddad4e7c4fac21cda0e4ab2da354e2098705c81
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-13 01:30:41 -0300

    Setting proper HTTP method for credentials.

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,8 @@ import ModelForUser from './models/UsersData';
 
 const app = express();
 
-// Rote to index
-app.get("/confirmLogin", async (req: Request, resp: Response)=>{
+// Check credentials
+app.post("/confirmLogin", async (req: Request, resp: Response)=>{
     let userExist = await ModelForUser.exists({user: req.query.user, password: req.query.password})
 
     if (userExist) {
