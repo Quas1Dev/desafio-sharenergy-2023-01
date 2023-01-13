1a4892c0b9d29f643080efca3e9f6af2f70fd696
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-13 01:23:10 -0300

    Set server to confirm credentials.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,21 @@
 import express from 'express';
+import { Request, Response } from "express";
+
 // This guarantee the dbConfig content gets executed
 import './config/dbConfig';
+import ModelForUser from './models/UsersData';
 
 const app = express();
 
 // Rote to index
-app.get("/confirmLogin", (req, resp)=>{
+app.get("/confirmLogin", async (req: Request, resp: Response)=>{
+    let userExist = await ModelForUser.exists({user: req.query.user, password: req.query.password})
 
-    const serverEndpoint = "l";
-    console.log("Acessou login.");
+    if (userExist) {
+        return resp.json({isRegistered: true})
+    }
 
-    return resp.json([
-        {id: 1, name: "Anúncio 1"},
-        {id: 2, name: "Anúncio 2"},
-        {id: 3, name: "Anúncio 3"},
-    ]);
+    return resp.json({isRegistered: false});
 })
 
 app.listen(3333);
