a387c9148e1f2c6f1b641cdca5221c3df562c706
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-14 06:19:50 -0300

    Added to the page with a list of random users..

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import LoginForm from './components/loginform';
+import RandomUser from './components/randomuser';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LoginForm/>}/>
-          <Route path="/randomuser" element={<LoginForm/>}/>
-          <Route path="/httpimage" element={<LoginForm/>}/>
+          <Route path="/randomuser" element={<RandomUser/>}/>
+          <Route path="/httpimage" element={<RandomUser/>}/>
           <Route path="/randomDogs" element={<LoginForm/>}/>
           <Route path="/customers" element={<LoginForm/>}/>
         </Routes>
