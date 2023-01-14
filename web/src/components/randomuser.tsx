12c078527fe180f8bda59687cc4804776b34e211
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-14 07:25:23 -0300

    Added classes to title and description

--- web/src/components/randomuser.tsx
+++ web/src/components/randomuser.tsx
@@ -5,8 +5,8 @@ export default function RandomUser() {
         <div className="page_container--random_user_page">
             <Navigation/>
             <main>
-                <h1>Lista de Usuários</h1>
-                <p>As informações nessa lista de usuário foram geradas automáticamente usando a API  Random User Generator. Você pode usar a caixa de pesquisa para  procurar  por usuários especificos na lista.</p>
+                <h1 className="random_user_page--page_title">Lista de Usuários</h1>
+                <p className="random_user_page--page_description">As informações nessa lista de usuário foram geradas automáticamente usando a API  Random User Generator. Você pode usar a caixa de pesquisa para  procurar  por usuários especificos na lista.</p>
             </main>
         </div>
     )
