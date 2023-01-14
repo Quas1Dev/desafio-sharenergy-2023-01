362310bc2b4836733217f8a5190337263ce8410f
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-14 07:11:45 -0300

    Initial navigation

---
+++ web/src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+export default function Navigation() {
+    return (
+        <nav className="site_menu">
+            <ul className="site_menu--list_of_links">
+                <li><Link to="/randomusers" className="list_of_links--link" > Lista de Usuários </Link></li>
+                <li><Link to="/randomusers" className="list_of_links--link"> Gerador de imagem HTTP </Link></li>
+                <li><Link to="/randomusers" className="list_of_links--link"> Cachorro aleatório </Link></li>
+                <li><Link to="/randomusers" className="list_of_links--link"> CRUID </Link></li>
+            </ul>
+        </nav>
+    )
+}
