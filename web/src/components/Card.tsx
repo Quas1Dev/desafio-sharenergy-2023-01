ceead39dfe62092c85b98b4d8c48a37ec09bff14
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-14 11:36:35 -0300

    Card added.

---
+++ web/src/components/Card.tsx
@@ -0,0 +1,27 @@
+export interface RandomUserData {
+    name: string;
+    email: string;
+    image: string;
+    username: string;
+    age: Number;
+    gender: string;
+    key: string;
+}
+
+export default function Card(props: RandomUserData) {
+    return (
+        <div className="card_listing--user_card">
+            <div className="user_card--user_image">
+
+                <img src={props.image} alt="Foto do usuário." />
+
+            </div>
+            <div className="user_card--user_infos">
+                <span className="user_infos--info">{"Nome: " + props.name}</span>
+                <span className="user_infos--info">{"E-mail: " + props.email}</span>
+                <span className="user_infos--info">{"Usuário: "+props.username}</span>
+                <span className="user_infos--info">{"Idade: " + props.age}</span>
+            </div>
+        </div>
+    )
+}
