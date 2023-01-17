9059029f3e522fb2f19f23b724ebac392b75cf97
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-17 01:30:09 -0300

    Finished HttpImage component.

--- web/src/components/HttpImage.tsx
+++ web/src/components/HttpImage.tsx
@@ -1,13 +1,53 @@
+import axios from "axios";
+import Navigation from './global-components/Navigation';
+import { ChangeEvent, useEffect, useState } from "react";
+
 export default function HttpImage() {
+    const [pickedCode, setPickedCode] = useState(0);
+    const [image, setIamge] = useState();
+
     const httpCodes = []
 
     for (let i = 100; i < 600; i++) {
-        <div> {httpCodes.push(i)} </div>
+        httpCodes.push(<option key={i} value={i} className="random_http_image--code_options"> {i} </option>)
     }
 
+    function handleChange(params: ChangeEvent<HTMLInputElement>) {
+        const { value } = params.target;
+        setPickedCode(Number(value));
+    }
+
+
     return (
-        <div className="page_container--random_http_image">
-            {httpCodes}
+        <div className="page_container--http_image_page">
+            <Navigation />
+            <main className="http_image_page--main_conent">
+
+                <h1 className="main_content--page_title">Gerador de imagem HTTP</h1>
+
+                <form action="#" className="page_conent--image_form">
+                    <label htmlFor="http_code_number" className="">Digite um código:  </label>
+                    <input type="number"
+                        min="2"
+                        max="100"
+                        name="http_code_picker"
+                        id="http_code_number"
+                        value={pickedCode}
+                        onChange={handleChange}
+                        className="random_http_image_page--http_code_picker" />
+                </form>
+
+                {/* <label htmlFor="http_code_picker">Digite um código:  </label>
+            <select
+                id="http_code_picker"
+                value={pickedCode}
+                onChange={handleChange}
+                name="favColor" 
+                className="random_http_image_page--http_code_picker">
+                {httpCodes}
+            </select> */}
+                <img src={"https://http.cat/" + pickedCode} alt="" className="random_http_image_page--http_image" />
+            </main>
         </div>
     )
 }
