5256c02d02799dbc6c33d284c83db81c266d6834
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-17 08:50:49 -0300

    Removed import of unused interfaces.

--- web/src/components/Clients.tsx
+++ web/src/components/Clients.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FormEvent, ChangeEvent, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ReactModal from 'react-modal';
 import Navigation from "./global-components/Navigation"
 import AddUserForm from "./clients-components/AddUserForm";
