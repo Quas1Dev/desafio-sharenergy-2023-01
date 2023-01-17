987aecc1efafca8fefdc7378e2282b91d1b78eee
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-17 09:14:06 -0300

    Removed unnecessary comments.

--- web/src/components/clients-components/AddUserForm.tsx
+++ web/src/components/clients-components/AddUserForm.tsx
@@ -49,11 +49,10 @@ export default function AddUserForm() {
     }
 
     function maskCpf(value: string): string {
-        value = value.replace(/\D/g, "")                    //Remove tudo o que não é dígito
-        value = value.replace(/(\d{3})(\d)/, "$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-        value = value.replace(/(\d{3})(\d)/, "$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-        //de novo (para o segundo bloco de números)
-        value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+        value = value.replace(/\D/g, "")                    
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")       
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")       
+        value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2") 
         return value
     }
     return (
