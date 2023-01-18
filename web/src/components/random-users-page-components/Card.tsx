6c0bdd2e156b1551390aadd7a3c860536cc331d4
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-18 00:54:17 -0300

    Destructured props and implemented alternative classes for cards based on gender property of user object.

--- web/src/components/random-users-page-components/Card.tsx
+++ web/src/components/random-users-page-components/Card.tsx
@@ -8,19 +8,24 @@ export interface RandomUserData {
     key: string;
 }
 
-export default function Card(props: RandomUserData) {
+export default function Card({ name, email, image, username, age, gender, key }: RandomUserData) {
+    // console.log("Gen:",gender)
     return (
-        <div className="card_listing--user_card">
+        <div className={"card_listing--user_card " +
+            (gender == "female" ?
+            "user_card-female" :
+            "user_card-male")}>
+
             <div className="user_card--user_image">
 
-                <img src={props.image} alt="Foto do usuário." />
+                <img src={image} alt="Foto do usuário." />
 
             </div>
             <div className="user_card--user_infos">
-                <span className="user_infos--info">{"Nome: " + props.name}</span>
-                <span className="user_infos--info">{"E-mail: " + props.email}</span>
-                <span className="user_infos--info">{"Usuário: " + props.username}</span>
-                <span className="user_infos--info">{"Idade: " + props.age}</span>
+                <span className="user_infos--info">{"Nome: " + name}</span>
+                <span className="user_infos--info">{"E-mail: " + email}</span>
+                <span className="user_infos--info">{"Usuário: " + username}</span>
+                <span className="user_infos--info">{"Idade: " + age}</span>
             </div>
         </div>
     )
