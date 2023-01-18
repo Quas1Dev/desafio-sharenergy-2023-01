2f0449f8f82938b0e4b8cecc2c619067f3c8065c
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-18 07:34:04 -0300

    Moved fields to their own file.

--- web/src/components/clients-components/AddUserForm.tsx
+++ web/src/components/clients-components/AddUserForm.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import FormFields from "./FormFields";
 import { FormEvent, ChangeEvent, useState } from 'react';
 
 interface AddUserFormPropsinterface {
@@ -16,6 +17,7 @@ interface AddUserFormInterface {
 }
 
 export default function AddUserForm({ setIsOpen, fetchClients }: AddUserFormPropsinterface) {
+
     const [addUserForm, setAddUserForm] = useState<AddUserFormInterface>({
         name: "",
         email: "",
@@ -87,55 +89,14 @@ export default function AddUserForm({ setIsOpen, fetchClients }: AddUserFormProp
 
     return (
         <>
-        <span className="modal_add_user--add_user_form_warning" style={{
-            display: emptyField ? "block" : "none"
-        }}>Por favor, preencha todos os campos obrigatórios</span>
+            <span className="modal_add_user--add_user_form_warning" style={{
+                display: emptyField ? "block" : "none"
+            }}>Por favor, preencha todos os campos obrigatórios</span>
 
-        <form action="" className="modal_add_user--add_user_form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Nome</label>
-            <input type="text"
-                id="name"
-                name="name"
-                onChange={handleChange}
-                className="add_user_form--field"
-                value={addUserForm.name} />
-
-            <label htmlFor="email">E-mail</label>
-            <input type="text"
-                id="email"
-                name="email"
-                onChange={handleChange}
-                className="add_user_form--field"
-                value={addUserForm.email} />
-
-            <label htmlFor="telephone">Telefone</label>
-            <input type="tel"
-                id="telephone"
-                name="telephone"
-                maxLength={15}
-                onChange={handleChange}
-                className="add_user_form--field"
-                value={addUserForm.telephone} />
-
-            <label htmlFor="cpf">CPF</label>
-            <input type="text"
-                id="cpf"
-                name="cpf"
-                maxLength={14}
-                onChange={handleChange}
-                className="add_user_form--field"
-                value={addUserForm.cpf} />
-
-            <label htmlFor="address">Endereço</label>
-            <input type="text"
-                id="address"
-                name="address"
-                onChange={handleChange}
-                className="add_user_form--field"
-                value={addUserForm.address} />
-
-            <button className="add_user_form--submit">Adicionar</button>
-        </form>
+            <form action="" className="modal_add_user--add_user_form" onSubmit={handleSubmit}>
+                <FormFields />
+                <button className="add_user_form--submit">Adicionar</button>
+            </form>
         </>
     )
 }
