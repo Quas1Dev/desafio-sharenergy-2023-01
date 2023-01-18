4cdca40c2b327e669a08261bd782d1d08bc7e568
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-18 01:30:55 -0300

    Using 'beautify' to rearrange the code.

--- web/src/components/Clients.tsx
+++ web/src/components/Clients.tsx
@@ -17,7 +17,7 @@ export default function Clients() {
     const [clients, setClients] = useState<ClientsInterface[]>([]);
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    
+
     const fetchClients = async () => {
         const response = await axios.get("http://localhost:3333/read");
         setClients(response.data);
@@ -27,9 +27,9 @@ export default function Clients() {
         fetchClients()
     }, []);
 
-    function handleClickDelete(cpf : string) { 
-        const deleteClient = async()=>{
-            const response = await axios.delete("http://localhost:3333/delete/?cpf="+cpf);
+    function handleClickDelete(cpf: string) {
+        const deleteClient = async () => {
+            const response = await axios.delete("http://localhost:3333/delete/?cpf=" + cpf);
             fetchClients();
         }
         deleteClient();
@@ -45,7 +45,8 @@ export default function Clients() {
         <main className="page_container--main_ncontent">
             <Navigation />
             <h1 className="main_content--clients_page_title u-title">Clientes</h1>
-
+            <p className="page_content--page_description">A tabela abaixo mostra os dados de clientes que cadastramos em nosso banco de dados hospedado na núvem. Caso  nenhum cliente esteja cadastrado, um botão
+                apenas adicione um novo usuário.</p>
             <button className="clients_page--add_user" onClick={handleOpenCloseModal}>Adicionar usuário</button>
 
             <ReactModal
@@ -55,7 +56,7 @@ export default function Clients() {
                 isOpen={isOpen}
                 onRequestClose={handleOpenCloseModal}
             >
-                <AddUserForm setIsOpen={setIsOpen} fetchClients={fetchClients}/>
+                <AddUserForm setIsOpen={setIsOpen} fetchClients={fetchClients} />
             </ReactModal>
 
             <main>
@@ -72,21 +73,21 @@ export default function Clients() {
                             </tr>
                         </thead>
                         <tbody>
-                        {clients.map(client => {
-                            return (
-                                <tr key={client.cpf}>
-                                    <td>{client.name}</td>
-                                    <td>{client.telephone}</td>
-                                    <td>{client.email}</td>
-                                    <td>{client.cpf}</td>
-                                    <td>{client.address}</td>
-                                    <td>
-                                        <Pencil />
-                                        <XCircle onClick={(e) => handleClickDelete(client.cpf)}/>
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                            {clients.map(client => {
+                                return (
+                                    <tr key={client.cpf}>
+                                        <td>{client.name}</td>
+                                        <td>{client.telephone}</td>
+                                        <td>{client.email}</td>
+                                        <td>{client.cpf}</td>
+                                        <td>{client.address}</td>
+                                        <td>
+                                            <Pencil />
+                                            <XCircle onClick={(e) => handleClickDelete(client.cpf)} />
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
                 }
