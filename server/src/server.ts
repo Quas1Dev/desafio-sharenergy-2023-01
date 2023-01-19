70964479994675d05c648996ca83e43a0e1705c2
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-19 14:07:55 -0300

    Fixed error on add client.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,81 +1,11 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
-
-// This guarantee the dbConfig content gets executed
-import './config/dbConfig';
-import ModelForUser from './models/UsersData';
-import ModelForClient from './models/ClientsData';
-import axios, { AxiosError } from 'axios';
-
+import './config/dbConfig'; // Config is executed
+import routes from './routes/routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Check credentials
-app.post("/confirmLogin", async (req: Request, resp: Response) => {
-    const { user, password } = req.query;
-    const userExist = await ModelForUser.exists({ user, password });
-
-    return resp.json(userExist);
-});
-
-// Add new client
-app.post("/add", async (req: Request, resp: Response) => {
-    const { name, email, telephone, cpf, address } = req.body;
-
-    // Do not allow duplicate clients
-    const client = await ModelForClient.findOne({ cpf });
-
-    if (!client) return resp.json({ userAdded: false });
-
-    const newClient = new ModelForClient({
-        name,
-        cpf,
-        email,
-        telephone,
-        address
-    });
-
-    const savedDoc = await newClient.save();
-
-    if (savedDoc == newClient) {
-        resp.json({ userAdded: true })
-    } else {
-        resp.json({ userAdded: false });
-    }
-})
-
-app.get("/read", async (req: Request, resp: Response) => {
-    const clients = await ModelForClient.find({});
-    return resp.json(clients)
-})
-
-app.delete("/delete/:id", async (req: Request, resp: Response) => {
-    const query = await ModelForClient.deleteOne({ _id: req.params.id });
-    resp.json(query);
-})
-
-app.put('/update/:id', async (req: Request, resp: Response) => {
-    
-    const id = req.params.id;
-    const { cpf, email, telephone, address, name } = req.body;
-
-    const client = await ModelForClient.findById(id);
-
-    if (!client) return resp.json(client);
-
-    if (cpf || email || telephone || address || name) {
-        client.cpf = cpf;
-        client.name = name;
-        client.email = email;
-        client.address = address;
-        client.telephone = telephone;
-
-        client.save();
-    }
-
-    return resp.json(client);
-})
+app.use(routes);
 
 app.listen(3333);
