934df1c2dfee5f47b6bfc5689090929e65ec6163
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-20 06:19:13 -0300

    User state added to App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,19 +3,23 @@ import RandomUser from './components/RandomUsers';
 import HttpImage from './components/HttpImage';
 import RandomDog from './components/RandomDog';
 import Clients from './components/Clients';
-
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// User id for checking login goes here.
+// Avoid using context.
+const [user, setUser] = useState<string>("");
 
 function App() {
   return (
     <div className="page_container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginForm/>}/>
-          <Route path="/randomuser" element={<RandomUser/>}/>
-          <Route path="/httpimage" element={<HttpImage/>}/>
-          <Route path="/randomdog" element={<RandomDog/>}/>
-          <Route path="/clients" element={<Clients/>}/>
+          <Route path="/" element={<LoginForm user={user} setUser={setUser} />}/>
+          <Route path="/randomuser" element={<RandomUser user={user}/>}/>
+          <Route path="/httpimage" element={<HttpImage user={user}/>}/>
+          <Route path="/randomdog" element={<RandomDog user={user}/>}/>
+          <Route path="/clients" element={<Clients user={user}/>}/>
         </Routes>
       </BrowserRouter>
     </div>
