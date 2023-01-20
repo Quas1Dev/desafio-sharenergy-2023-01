d14e4dc83d10b49f84c7fae0962665c47d9f541a
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-20 15:19:38 -0300

    Added validation

--- web/src/components/clients-components/AddClientForm.tsx
+++ web/src/components/clients-components/AddClientForm.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import FormFields from "./FormFields";
 import { FormEvent, ChangeEvent, useState } from 'react';
-import { maskTelephone, maskCpf} from '../../utils/Masks';
+import { maskTelephone, maskCpf } from '../../utils/masks';
 import { AddClientFormPropsInterface, ClientInterface } from "../../interfaces/ClientsPageInterfaces";
+import validate, { isEmpty } from "../../utils/validation";
 
-
-export default function AddClientForm ({ setIsOpen, fetchClients }: AddClientFormPropsInterface) {
+export default function AddClientForm({ setIsOpen, fetchClients }: AddClientFormPropsInterface) {
 
     const [clientFormData, setClientFormData] = useState<ClientInterface>({
         name: "",
@@ -16,25 +16,15 @@ export default function AddClientForm ({ setIsOpen, fetchClients }: AddClientFor
         _id: ""
     })
 
-    const [emptyField, setEmptyField] = useState<boolean>(false)
+    const [imcorrectForm, setIncorrectForm] = useState<boolean>(false)
 
     function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        
-        let key: keyof ClientInterface;
 
-        let emptyValue: boolean = false;
-        console.log("Keys: ", Object.keys(clientFormData));
-        for (key of Object.keys(clientFormData)) {
-            // console.log("Key empty: ", key)
-            if (clientFormData[key] == "" && key !="_id") {
-                console.log("Key:", clientFormData[key])
-                setEmptyField(true);
-                emptyValue = true;
-            }
-        }
+        const empty: boolean = isEmpty(clientFormData);
+        const isValid = validate(clientFormData);
 
-        if (!emptyValue) {
+        if (!empty && isValid) {
             const addUser = async () => {
                 const response = await axios.post("http://localhost:3333/add", clientFormData);
                 setIsOpen(false);
@@ -42,6 +32,8 @@ export default function AddClientForm ({ setIsOpen, fetchClients }: AddClientFor
             }
 
             addUser();
+        } else {
+            setIncorrectForm(true);
         }
     }
 
@@ -67,11 +59,11 @@ export default function AddClientForm ({ setIsOpen, fetchClients }: AddClientFor
     return (
         <>
             <span className="modal_add_user--add_user_form_warning" style={{
-                display: emptyField ? "block" : "none"
-            }}>Por favor, preencha todos os campos obrigatórios</span>
+                display: imcorrectForm ? "block" : "none"
+            }}>Por favor, preencha todos os campos com dados válidos!</span>
 
             <form action="" className="modal_add_user--add_user_form" onSubmit={handleSubmit}>
-                <FormFields handleChange={handleChange} clientData={clientFormData}/>
+                <FormFields handleChange={handleChange} clientData={clientFormData} />
                 <button className="add_user_form--submit">Adicionar</button>
             </form>
         </>
---
+++ web/src/utils/validation.tsx
@@ -0,0 +1,81 @@
+import { ClientInterface } from "../interfaces/ClientsPageInterfaces";
+
+export function isEmpty (obj: ClientInterface) : boolean {
+    let key: keyof ClientInterface;
+
+    for (key of Object.keys(obj)) {
+        if (obj[key] == "" && key != "_id") {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export default function validate(obj: ClientInterface) {
+    const cpfValid: boolean = checkCpf(obj.cpf);
+    const telephoneValid: boolean = checkTelephone(obj.telephone);
+
+    return cpfValid && telephoneValid;
+}
+
+function checkCpf(strCPF: string): boolean {
+    strCPF = strCPF.replace(/\D/g, '');
+
+    var sum: number;
+    var remainder: number;
+    sum = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (let i: number = 1; i <= 9; i++) {
+        sum = sum + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+
+    if ((remainder == 10) || (remainder == 11)) {
+        remainder = 0;
+    }
+
+    if (remainder != parseInt(strCPF.substring(9, 10))) {
+        return false;
+    }
+
+    sum = 0;
+    for (let i: number = 1; i <= 10; i++) {
+        sum = sum + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    }
+    remainder = (sum * 10) % 11;
+
+    if ((remainder == 10) || (remainder == 11)) remainder = 0;
+    if (remainder != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+}
+
+function checkTelephone(phone: string) {
+    phone = phone.replace(/\D/g, '');
+
+    if (!(phone.length >= 10 && phone.length <= 11)) return false;
+
+    if (phone.length == 11 && parseInt(phone.substring(2, 3)) != 9) return false;
+
+    for (var i: number = 0; i < 10; i++) {
+        if (phone == new Array(11).join("" + i) || phone == new Array(12).join("" + i)) return false;
+    }
+
+    var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+        21, 22, 24, 27, 28, 31, 32, 33, 34,
+        35, 37, 38, 41, 42, 43, 44, 45, 46,
+        47, 48, 49, 51, 53, 54, 55, 61, 62,
+        64, 63, 65, 66, 67, 68, 69, 71, 73,
+        74, 75, 77, 79, 81, 82, 83, 84, 85,
+        86, 87, 88, 89, 91, 92, 93, 94, 95,
+        96, 97, 98, 99];
+
+    if (codigosDDD.indexOf(parseInt(phone.substring(0, 2))) == -1) return false;
+
+    if (new Date().getFullYear() < 2017) return true;
+    if (phone.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(phone.substring(2, 3))) == -1) return false;
+
+    return true;
+}
