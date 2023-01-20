b2d780439367123362db043a066230a856e8bc8b
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-20 11:44:06 -0300

    Conditionally running routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,23 +3,37 @@ import RandomUser from './components/RandomUsers';
 import HttpImage from './components/HttpImage';
 import RandomDog from './components/RandomDog';
 import Clients from './components/Clients';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-// User id for checking login goes here.
-// Avoid using context.
-const [user, setUser] = useState<string>("");
+import axios from 'axios';
 
 function App() {
+  // User id for checking login goes here.
+  // Avoid using context.
+  const [user, setUser] = useState<string>("");
+  const [userConfirmed, setUserConfirmed] = useState<boolean>(false);
+  useEffect(() => {
+    const checkUser = async () => {
+      const response = await axios.get("http://localhost:3333/confirmUser/" + JSON.parse(user)._id);
+      if (response.data._id) {
+        setUserConfirmed(true);
+      }
+    }
+    checkUser();
+  }, [user])
   return (
     <div className="page_container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginForm user={user} setUser={setUser} />}/>
-          <Route path="/randomuser" element={<RandomUser user={user}/>}/>
-          <Route path="/httpimage" element={<HttpImage user={user}/>}/>
-          <Route path="/randomdog" element={<RandomDog user={user}/>}/>
-          <Route path="/clients" element={<Clients user={user}/>}/>
+          <Route path="/" element={<LoginForm user={user} setUser={setUser} />} />
+          {userConfirmed && 
+           <>
+          <Route path="/randomuser" element={<RandomUser user={user} setUser={setUser} />} />
+          <Route path="/httpimage" element={<HttpImage user={user} setUser={setUser} />} />
+          <Route path="/randomdog" element={<RandomDog user={user} setUser={setUser} />} />
+          <Route path="/clients" element={<Clients user={user} setUser={setUser} />} />
+          </>
+          }
         </Routes>
       </BrowserRouter>
     </div>
