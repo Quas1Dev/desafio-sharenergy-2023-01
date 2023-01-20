e647492b5b4373712ab7047a97c917dc296160fe
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-20 11:42:08 -0300

    Fixing & preventing error on id -> ObjectId

--- server/src/controllers/UserControllers.ts
+++ server/src/controllers/UserControllers.ts
@@ -1,11 +1,21 @@
-import ModelForUser from "../models/ClientsData";
-import {Request, Response} from 'express';
+import ModelForUser from "../models/UsersData";
+import { Request, Response } from 'express';
 
 export default {
-    async confirmLogin (req: Request, resp: Response) {
-        const { user, password } = req.query;
+    async confirmLogin(req: Request, resp: Response) {
+        const { user, password } = req.body;
         const userExist = await ModelForUser.exists({ user, password });
-    
-        return resp.json(userExist);
+        return userExist ? resp.json(userExist) : resp.json({_id : null});
+    },
+
+    async confirmUser(req: Request, resp: Response) {
+        const { id } = req.params;
+        try {
+            const userExist = await ModelForUser.exists({ _id: id });
+            return userExist ? resp.json(userExist) : resp.json({_id : null})
+        } catch (err) {
+            return resp.json({_id: null});
+        }
+
     }
 }
