1ee81405e6cd4e75a8be119ca8fbda8cc86fa2a8
Fernando Bonfim <devnoob@vivaldi.net>
2023-01-24 10:03:27 -0300

    Cleaner code.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,53 +3,44 @@ import RandomUser from './components/RandomUsers';
 import HttpImage from './components/HttpImage';
 import RandomDog from './components/RandomDog';
 import Clients from './components/Clients';
+
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+
+import api from './axiosInstance';
+import ProtectedRoute from './components/utils-components/ProtectedComponents';
+import { UserInterface } from './interfaces/GlobalInterface';
 
 function App() {
   // User id for checking login goes here.
-  // Avoid using context.
   const [user, setUser] = useState<string>(JSON.stringify({ _id: null }));
-  const [userConfirmed, setUserConfirmed] = useState<boolean>(false);
-
-  useEffect(() => {
-    const checkUser = async () => {
-      const response = await axios.get("http://localhost:3333/confirmUser/" + JSON.parse(user)._id);
-      if (response.data._id) {
-        setUserConfirmed(true);
-      }
-    }
-    checkUser();
-  }, [user])
 
   /* 
      This should make the user "always on" *1
    */
   useEffect(() => {
-    let loggedUser = localStorage.getItem("user") || sessionStorage.getItem('user');
-    console.log("Logged:", loggedUser)
-    if (loggedUser) {
-      setUser(loggedUser);
-    } else {
-      console.log("Null!")
-      setUser(JSON.stringify({ _id: null }));
+    const loadAndCheckUser = async () => {
+      let storedUser : UserInterface = JSON.parse(localStorage.getItem("user") || sessionStorage.getItem('user') || "{'_id': null }");
+      const response = await api.get<UserInterface>("/confirmUser/" + storedUser._id);
+
+      if (response.data._id) setUser(JSON.stringify(storedUser));
     }
+
+    loadAndCheckUser();
   }, [])
 
   return (
     <div className="page_container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login user={user} setUser={setUser} />} />
-          {userConfirmed &&
-            <>
-              <Route path="/randomuser" element={<RandomUser />} />
-              <Route path="/httpimage" element={<HttpImage />} />
-              <Route path="/randomdog" element={<RandomDog />} />
-              <Route path="/clients" element={<Clients />} />
-            </>
-          }
+          <Route index element={<Login user={user} setUser={setUser} />} />
+          <Route element={<ProtectedRoute user={JSON.parse(user)} redirectPath="/" />}>
+            <Route path="/randomuser" element={<RandomUser />} />
+            <Route path="/httpimage" element={<HttpImage />} />
+            <Route path="/randomdog" element={<RandomDog />} />
+            <Route path="/clients" element={<Clients />} />
+          </Route>
+          <Route path="*" element={<p>Nada por aqui: 404</p>} />
         </Routes>
       </BrowserRouter>
     </div>
--- web/src/interfaces/GlobalInterface.tsx
+++ web/src/interfaces/GlobalInterface.tsx
@@ -2,3 +2,7 @@ export interface GlobalPropsInterface {
     user: string,
     setUser: Function
 }
+
+export interface UserInterface {
+    _id : string | null;
+}
